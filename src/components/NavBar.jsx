01461db249010219ebe1cec040d532574c080e4f
masud467 <masudpfrnt@gmail.com>
2025-01-09 16:54:36 +0600

    added NavBar setup.

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,17 @@
+import styles from "../app/styles/NabVar.module.css";
+
+const NavBar = () => {
+  return (
+    <div className={styles.container}>
+      <header className={styles.header}>
+        <div className={styles.logo}>ExpenseTracker</div>
+        <nav className={styles.nav}>
+          <a href="/">Home</a>
+          <a href="/summary">Summary</a>
+        </nav>
+      </header>
+    </div>
+  );
+};
+
+export default NavBar;
